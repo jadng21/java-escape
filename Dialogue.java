eaad5427555e80078f920bb03600e702c04ae413
Noela <jade.ngoma@ynov.com>
2026-04-22 15:18:22 +0200

    etape 1 jade

---
+++ Dialogue.java
@@ -0,0 +1,25 @@
+package com.example.javaescape;
+
+public class Dialogue {
+    private String personnage;
+    private String texte;
+    private String imagePath;
+
+    public Dialogue ( String personnage, String texte, String imagePath) {
+        this.personnage = personage;
+        this.texte = texte;
+        this.imagePath = imagePath;
+    }
+
+    public String getPersonnage() {
+        return personnage;
+    }
+
+    public String getTexte() {
+        return texte;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+}
