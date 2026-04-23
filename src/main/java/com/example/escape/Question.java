75e5313774c9e2c745a0ce539c8be1e2382a6692
rubit <rubitombe1@gmail.com>
2026-04-23 18:25:01 +0200

    fin question et erreurs

--- src/main/java/com/example/escape/Question.java
+++ src/main/java/com/example/escape/Question.java
@@ -1,58 +1,61 @@
+package com.example.escape;
+
 import com.example.escape.QuizApi;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
-import com.example.escape;
+
+
 
 public class Question {
-    private String enonce;
-    private String answer;
+    private String question;
+    private String correct_answer;
     private String difficulty;
     private String category;
     private List<String> options;
 
     public Question(JSONObject jsonQuestion) {
-        this.enonce = QuizApi.decodeHtml(
-                jsonQuestion.getString("enonce")
-        );
-        this.answer = QuizApi.decodeHtml(
-                jsonQuestion.getString("answer")
-        );
-        this.difficulty = QuizApi.decodeHtml(
-                jsonQuestion.getString("difficulty")
-        );
-        this.category = QuizApi.decodeHtml(
-                jsonQuestion.getString("category")
-        );
+        this.question = QuizApi.decodeHtml(jsonQuestion.getString("question"));
+        this.correct_answer = QuizApi.decodeHtml(jsonQuestion.getString("correct_answer"));
+        this.difficulty = QuizApi.decodeHtml(jsonQuestion.getString("difficulty"));
+        this.category = QuizApi.decodeHtml(jsonQuestion.getString("category"));
 
         this.options = new ArrayList<>();
-        this.options.add(answer);
+        this.options.add(this.correct_answer);
 
-        JSONArray false = jsonQuestion.getJSONArray("false");
-        for (int i = 0; i < false.length(); i ++){
-            this.options.add(QuizApi.decodeHtml(false.getString(i)));
-        }
-
-        public boolean verifAnswer(String options){
-            return options != null && options.equals(answer);
+        JSONArray incorrect_answers = jsonQuestion.getJSONArray("incorrect_answers");
+        for (int i = 0; i < incorrect_answers.length(); i++) {
+            this.options.add(QuizApi.decodeHtml(incorrect_answers.getString(i)));
         }
 
-        public String getEnonce(){
-            return enonce;
-        }
-        public List<String> getOptions(){
-            return options;
-        }
-        public String getDifficulty(){
-            return difficulty;
-        }
-        public String getCategory(){
-            return category;
-        }
-        public String getAnswer(){
-            return answer;
-        }
+        Collections.shuffle(this.options);
+    }
+
+    public boolean verifAnswer(String options) {
+        return options != null && options.equals(correct_answer);
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getCorrect_answer() {
+        return correct_answer;
     }
 }
