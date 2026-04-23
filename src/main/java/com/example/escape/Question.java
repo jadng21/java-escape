92687b34511c75f1c3124c90c3c243c1c2bdc8b0
rubit <rubitombe1@gmail.com>
2026-04-23 16:51:39 +0200

    question fin avec erreur

--- src/main/java/com/example/escape/Question.java
+++ src/main/java/com/example/escape/Question.java
@@ -1,11 +1,58 @@
-
-
-
-
+import com.example.escape.QuizApi;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.List;
+import java.util.ArrayList;
+import com.example.escape;
 
 public class Question {
-    private String question;
-    private
+    private String enonce;
     private String answer;
-    public Question(String question, String answer) {}
+    private String difficulty;
+    private String category;
+    private List<String> options;
+
+    public Question(JSONObject jsonQuestion) {
+        this.enonce = QuizApi.decodeHtml(
+                jsonQuestion.getString("enonce")
+        );
+        this.answer = QuizApi.decodeHtml(
+                jsonQuestion.getString("answer")
+        );
+        this.difficulty = QuizApi.decodeHtml(
+                jsonQuestion.getString("difficulty")
+        );
+        this.category = QuizApi.decodeHtml(
+                jsonQuestion.getString("category")
+        );
+
+        this.options = new ArrayList<>();
+        this.options.add(answer);
+
+        JSONArray false = jsonQuestion.getJSONArray("false");
+        for (int i = 0; i < false.length(); i ++){
+            this.options.add(QuizApi.decodeHtml(false.getString(i)));
+        }
+
+        public boolean verifAnswer(String options){
+            return options != null && options.equals(answer);
+        }
+
+        public String getEnonce(){
+            return enonce;
+        }
+        public List<String> getOptions(){
+            return options;
+        }
+        public String getDifficulty(){
+            return difficulty;
+        }
+        public String getCategory(){
+            return category;
+        }
+        public String getAnswer(){
+            return answer;
+        }
+    }
 }
